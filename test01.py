970d3f72690b77d31bec0c9d07ee1cff13aa840c
StevenZsh <1558785107@qq.com>
2019-03-14 21:18:13 +0800

    第一次提交

---
+++ test01.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+@version: ?
+@author: StevenZhang
+@contact: 1558785107@qq.com
+@software: PyCharm
+@file: test01.py
+@time: 2018/12/10 20:07
+"""
+
+import sys,os
+
+print(sys.path)
+
+print(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
